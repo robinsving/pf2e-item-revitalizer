8a6cfdde4ad820b5c69a85bd9a6ff360c873acf8
Robin Sving <robin.sving@gmail.com>
2023-06-16 22:56:49 +0200

    Removing patterns for false positives, eslinter

--- src/pf2e-item-revitalizer-check.js
+++ src/pf2e-item-revitalizer-check.js
@@ -8,7 +8,7 @@ const langFile = `modules/your-module/lang/${language}.json`; // Adjust the path
 // Load the language file or fallback to default language strings
 const i18n = game.i18n.translations[language]?.COMPATIBILITY_CHECK || defaultLang.COMPATIBILITY_CHECK;
 */
-const SCRIPT_NAME = "Revitalizer Check for PF2E"; // `${i18n.SCRIPT_NAME}`
+const SCRIPT_NAME = "PF2e Item Revitalizer"; // `${i18n.SCRIPT_NAME}`
 
 // Allowlist of properties to include in the clone
 const PF2E_PROPERTY_ALLOW_LIST = {
@@ -31,6 +31,7 @@ const PF2E_PROPERTY_ITEMS = ["action", "ancestry", "armor", "background", "backp
 
 // List of Items to ignore
 const PF2E_IGNORABLE_ITEM_UUIDS = [
+    "Compendium.pf2e.equipment-srd.UJWiN0K3jqVjxvKk", // Wand, lvl 2
     "Compendium.pf2e.equipment-srd.Y7UD64foDbDMV9sx" // Scroll, lvl 2
 ];
 
@@ -97,18 +98,18 @@ function getDifferentiatingProperties(originItem, actorItem) {
         
         /**
          * Create the JSON strings, but replace style formatting, as that may adjust spaces in browsers
-         * e.g. the difference of the following lines:
-         *   <span style=\"float:right\"> 
-         *   <span style=\"float: right;\">
+         * e.g. the differences of the following lines:
+         *   <span style=\"float:right\">  ->   <span style=\"float: right;\">
+         *   UUID[Compendium.pf2e.actionspf2e.KAVf7AmRnbCAHrkT]{Attack of Opportunity} -> UUID[Compendium.pf2e.actionspf2e.KAVf7AmRnbCAHrkT]
          */
-        const actorJson = JSON.stringify(actorItem[key]).replace(/style=\\".*\\"/);
-        const originJson = JSON.stringify(originItem[key]).replace(/style=\\".*\\"/);
+        const actorJson = JSON.stringify(actorItem[key]).replace(/style=\\".*\\"/).replace(/\{[^{}]*\}/);
+        const originJson = JSON.stringify(originItem[key]).replace(/style=\\".*\\"/).replace(/\{[^{}]*\}/);
         
         // If we find differences in the property
         if (actorJson !== originJson) {
             console.debug(`Found differences in ${key} for slug ${originItem.slug}:`);
-            console.debug(`Actor states: ${actorJson}`);
-            console.debug(`Compendium states: ${originJson}`);
+            console.debug(`Actor ${originItem.slug} states: ${actorJson}`);
+            console.debug(`Compendium ${originItem.slug} states: ${originJson}`);
             differentProperties.push(key);
         }
     }
@@ -169,7 +170,7 @@ for (const actor of actors) {
     
     // Iterate over the equipment
     for (const actorItem of actor.items.filter((item) => item.hasOwnProperty("type") && PF2E_PROPERTY_ITEMS.includes(item.type) && item.sourceId && item.sourceId !== null && !PF2E_IGNORABLE_ITEM_UUIDS.includes(item.sourceId))) {
-        console.debug(actorItem);
+        //console.debug(actorItem);
         // Check if the item has been changed
         const originItem = await fromUuid(actorItem.sourceId);
         
@@ -193,8 +194,9 @@ let output = "";
 if (changedData.length == 0) {
     console.log(actors)
     const searchedActors = actors.map(actor => actor.name).join(', ');
-    output = `<p>No changed items found for the following actors:<br>${searchedActors}</p>`;
+    output = `<h2>✅ No changed items found</h2><p>Searched through the following actors:<br>${searchedActors}</p>`;
 } else {
+
     /**
      * Create the formatted output in a table.
      * 
@@ -299,6 +301,14 @@ const dialogOptions = {
     default: 'ok',
 };
 
-const dialog = new Dialog(dialogOptions).render(true);
+// Render the Dialog, but on an error, inform the user that the CSS selector is not enabled, if applicable
+try {
+    const dialog = new Dialog(dialogOptions).render(true);
+} catch (e) {
+}
+if (!hasHasSelectorSupport) {
+    console.warn(`${SCRIPT_NAME} has detected that your browser is not allowing for the 'has' CSS-selector. The script's CSS has been adapted to adjust for this.
+This setting is a default for Firefox, 🦊, but it can be enabled in the 'about:config', under 'layout.css.has-selector.enabled'.`)
+}
 
 console.info(`Ending ${SCRIPT_NAME}`);
